ab32a8b5230444ce2bb2cc1e2abaa75001225424
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-24 18:45:59 +0300

    pg

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,14 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 // const { User } = require("../db/userModel");
+const { pool } = require("../db/connection");
 // const { NotAuthorizedError } = require("../helpers/errors");
 
 const registration = async (registrationData) => {
   const { first_name, last_name, email, phone, password } = registrationData;
   //   const user = new User({ email, password });
   console.log("email", email);
+  console.log("pool", pool);
 
   //   await user.save();
 };
