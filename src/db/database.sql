e0fa17beebfb218f06459a150c6c33bb5ba603e4
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-25 16:16:10 +0300

    Update database.sql

--- src/db/database.sql
+++ src/db/database.sql
@@ -2,17 +2,8 @@ CREATE TABLE users (
     id  SERIAL PRIMARY KEY,
     first_name VARCHAR(30),
 	last_name VARCHAR(30),
-    email VARCHAR(30),
+    email VARCHAR(30) UNIQUE,
 	phone VARCHAR(30),
     password VARCHAR(60),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- CREATE TABLE users (
---     id INT PRIMARY KEY,
---     first_name VARCHAR(30),
--- 	last_name VARCHAR(30),
---     email VARCHAR(30),
--- 	phone VARCHAR(30),
---     password VARCHAR(60),
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
