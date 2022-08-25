0e9b582017da5e00ea658b3b6e1c3062f952cb5c
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-25 19:45:55 +0300

    update by id

--- src/services/authService.js
+++ src/services/authService.js
@@ -44,11 +44,19 @@ const getUserById = async (id) => {
 };
 
 const updateUserById = async (id, updatedUserData) => {
-  const { first_name, last_name, email, phone, password } = updatedUserData;
+  const { first_name, last_name, email, phone } = updatedUserData;
+  const user = await pool.query(
+    `SELECT first_name, last_name, email, phone FROM users where id = $1`,
+    [id]
+  );
+
+  if (!user.rows[0]) {
+    throw new NotFoundError(`User with id: ${id} not found`);
+  }
 
   const updatedUser = await pool.query(
-    `UPDATE users set first_name = $1, last_name = $2, email = $3, phone = $4, password = $5 where id = $6 RETURNING *`,
-    [first_name, last_name, email, phone, password, id]
+    `UPDATE users set first_name = $1, last_name = $2, email = $3, phone = $4 where id = $5 RETURNING first_name, last_name, email, phone`,
+    [first_name, last_name, email, phone, id]
   );
 
   return updatedUser.rows[0];
